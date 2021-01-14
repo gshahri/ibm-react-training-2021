c2b13d4dbf97ff8339baef19540c860954ef7a5f
Magesh Kuppan <tkmagesh77@gmail.com>
2021-01-15 00:40:59 +0530

    using async await

--- 07-async/async-demos.js
+++ 07-async/async-demos.js
@@ -36,15 +36,48 @@
         console.log(`   [@service] processing ${x} and ${y}`);
         const p = new Promise(function(resolveFn, rejectFn){
             setTimeout(function(){
-                let result = x + y;
-                console.log(`   [@service] returning result`);
-                resolveFn(result);
+                try{
+                    let result = x + y;
+                    console.log(`   [@service] returning result`);
+                    resolveFn(result);                    
+                } catch (err){
+                    rejectFn(new Error('something went wrong'));
+                }
             },5000);
         });
         return p;
     }
 
-    
-    window['addAsyncPromise'] = addAsyncPromise;
+    function addAsyncPromiseClient(x,y){
+        console.log(`[@client] triggering the service`);
+        var p = addAsyncPromise(x,y);
+        p.then(function(result){
+            console.log(`[@client] result = ${result}`);
+        });
+    }
+
+    //promise delegation
+    /* function addAsyncPromiseClient(x,y){
+        console.log(`[@client] triggering the service`);
+        var p = addAsyncPromise(100,200);
+        handleResult(p);
+    }
+
+    function handleResult(p){
+        p.then(function(result){
+            console.log(`[@client] result = ${result}`);
+        });
+    } */
+
+   
+    //async await
+    async function addAsyncPromiseClient(x,y){
+        console.log(`[@client] triggering the service`);
+        var result = await addAsyncPromise(x,y);
+        console.log(`[@client] result = ${result}`);
+        return result * 2;
+    }
+  
+    window['addAsyncPromiseClient'] = addAsyncPromiseClient;
 
 })();
